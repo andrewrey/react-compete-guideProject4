7fedd07c585492235866e3132f0d74851289d2b4
Andrew Reynolds <andrew_reynolds@mac.com>
2021-06-26 11:33:51 -0700

    added card ui component

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,18 +1,21 @@
-import {useState} from 'react';
+import { useState } from "react";
+import Card from "../UI/Card";
+import styles from "./AddUser.module.css";
 
-const AddUser = () =>{
+const AddUser = () => {
   const addUserHandler = (event) => {
     event.preventDefault();
-
   };
-  return(
-    <form onSubmit={addUserHandler}>
-      <label htmlFor="username">Username</label>
-      <input id="name" type="text" />
-      <label htmlFor="age">Age (Years)</label>
-      <input id="age" type="number" />
-      <button type="submit">Add User</button>
-    </form>
+  return (
+    <Card className={styles.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input id="name" type="text" />
+        <label htmlFor="age">Age (Years)</label>
+        <input id="age" type="number" />
+        <button type="submit">Add User</button>
+      </form>
+    </Card>
   );
 };
 
