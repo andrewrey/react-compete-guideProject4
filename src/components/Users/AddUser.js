49cf467ea3caed2c729de17b258a1facde1a2469
Andrew Reynolds <andrew_reynolds@mac.com>
2021-06-26 12:08:34 -0700

    added button component and age validation on submit handler

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,19 +1,53 @@
 import { useState } from "react";
 import Card from "../UI/Card";
+import Button from "../UI/Button";
 import styles from "./AddUser.module.css";
 
 const AddUser = () => {
+  const [userName, setUserName] = useState("");
+  const [userAge, setUserAge] = useState("");
+
   const addUserHandler = (event) => {
     event.preventDefault();
+    if (userName.trim().length === 0 || userAge.trim().length === 0) {
+      return;
+    }
+    if (+userAge < 1) {
+      console.log("age too low");
+      return;
+    }
+
+    console.log(userName, userAge);
+    setUserName("");
+    setUserAge("");
   };
+
+  const onNameChangeHandler = (event) => {
+    setUserName(event.target.value);
+  };
+
+  const onAgeChangeHandler = (event) => {
+    setUserAge(event.target.value);
+  };
+
   return (
     <Card className={styles.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="name" type="text" />
+        <input
+          id="name"
+          type="text"
+          onChange={onNameChangeHandler}
+          value={userName}
+        />
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" />
-        <button type="submit">Add User</button>
+        <input
+          id="age"
+          type="number"
+          onChange={onAgeChangeHandler}
+          value={userAge}
+        />
+        <Button type="submit">Add User</Button>
       </form>
     </Card>
   );
