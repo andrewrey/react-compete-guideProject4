6e699ee1eecc7a2b701ba4647751059e52becf75
Andrew Reynolds <andrew_reynolds@mac.com>
2021-06-26 23:14:51 -0700

    used createPortal for moving backdrop and modal overlay

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,23 +1,40 @@
+import ReactDOM from "react-dom";
 import Card from "./Card";
 import Button from "./Button";
 import styles from "./Modal.module.css";
 
+const Backdrop = ({ onClick }) => {
+  return <div className={styles.backdrop} onClick={onClick}></div>;
+};
+
+const ModalOverlay = ({ message, title, onClose }) => {
+  return (
+    <Card className={styles.modal}>
+      <header className={styles.header}>
+        <h2>{title}</h2>
+      </header>
+      <div className={styles.content}>
+        <p>{message}</p>
+      </div>
+      <footer className={styles.actions}>
+        <Button onClick={onClose}>Close</Button>
+      </footer>
+    </Card>
+  );
+};
+
 const Modal = ({ title, message, onClose }) => {
   return (
-    <div>
-      <div className={styles.backdrop} onClick={onClose} />
-      <Card className={styles.modal}>
-        <header className={styles.header}>
-          <h2>{title}</h2>
-        </header>
-        <div className={styles.content}>
-          <p>{message}</p>
-        </div>
-        <footer className={styles.actions}>
-          <Button onClick={onClose}>Close</Button>
-        </footer>
-      </Card>
-    </div>
+    <>
+      {ReactDOM.createPortal(
+        <Backdrop onClick={onClose} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay message={message} title={title} onClose={onClose} />,
+        document.getElementById("modal-root")
+      )}
+    </>
   );
 };
 
